d33c8f8b16c3ba5c435acc7f4d34fc3020da7ed1
Alex Hutanu <valexhutanu@gmail.com>
2022-06-05 13:23:00 +0300

    Fixed typos

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -3,42 +3,42 @@ import NavBar from '../../components/NavBar/NavBar'
 
 export default () => {
    return (
-      <div className="landing-page__wrapper">
+      <div className="catalog-page__wrapper">
          <NavBar/>
-         <div className="landing-page">
-            <div className="landing-page__first-section">
+         <div className="catalog-page">
+            <div className="catalog-page__first-section">
                <img src='./Amandine.jpg' alt='cake' />
-               <div className='landing-page__first-section__category'>
-                  <p className='landing-page__first-section__category__name'>Brutărie </p>
+               <div className='catalog-page__first-section__category'>
+                  <p className='catalog-page__first-section__category__name'>Brutărie </p>
                </div>
-               <button className='landing-page__first-section__see-more-button' >
+               <button className='catalog-page__first-section__see-more-button' >
                   Vezi mai mult
                </button>
             </div>
-            <div className="landing-page__second-section">
+            <div className="catalog-page__second-section">
                <img src='https://chelsweets.com/wp-content/uploads/2021/11/blog-thumb-shot-of-white-drip-cake-2-819x1024.jpg.webp' alt='cake image' />
-               <div className='landing-page__second-section__category'>
-                  <p className='landing-page__second-section__category__name'>Torturi</p>
+               <div className='catalog-page__second-section__category'>
+                  <p className='catalog-page__second-section__category__name'>Torturi</p>
                </div>
-               <button className='landing-page__second-section__see-more-button' >
+               <button className='catalog-page__second-section__see-more-button' >
                   Vezi mai mult
                </button>
             </div>
-            <div className="landing-page__third-section">
+            <div className="catalog-page__third-section">
                <img src='./Cake.png' alt='cake' />
-               <div className='landing-page__third-section__category'>
-                  <p className='landing-page__third-section__category__name'>Prajituri</p>
+               <div className='catalog-page__third-section__category'>
+                  <p className='catalog-page__third-section__category__name'>Prajituri</p>
                </div>
-               <button className='landing-page__third-section__see-more-button' >
+               <button className='catalog-page__third-section__see-more-button' >
                   Vezi mai mult
                </button>
             </div>
-            <div className='landing-page__fourth-section'>
+            <div className='catalog-page__fourth-section'>
                <img src='https://wp.wedme.ro/wp-content/uploads/2020/06/Flavius-Partan-056.jpg' alt='cake' />
-               <div className='landing-page__fourth-section__category'>
-                  <p className='landing-page__fourth-section__category__name'>Candy Bar</p>
+               <div className='catalog-page__fourth-section__category'>
+                  <p className='catalog-page__fourth-section__category__name'>Candy Bar</p>
                </div>
-               <button className='landing-page__fourth-section__see-more-button' >
+               <button className='catalog-page__fourth-section__see-more-button' >
                   Vezi mai mult
                </button>
             </div>
