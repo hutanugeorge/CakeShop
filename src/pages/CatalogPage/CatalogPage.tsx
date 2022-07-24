4cd0be882d042cc6272386e03635696ef52680dd
Hutanu George <george@192-168-0-130.rdsnet.ro>
2022-07-24 19:25:19 +0300

    hotfix

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
 
 import Footer from '../../components/Footer'
 import NavBar from '../../components/NavBar/NavBar'
@@ -8,7 +8,7 @@ import ProductsShowcase from '../../components/ProductsShowcase'
 
 export default () => {
 
-   const [ showCakes, setShowCakes ] = useState<boolean>(false)
+   const [ showCakes, setShowCakes ] = useState<boolean>(true)
    const [ showCookies, setShowCookies ] = useState<boolean>(false)
    const [ showBakery, setShowBakery ] = useState<boolean>(false)
    const [ showIceCream, setShowIceCream ] = useState<boolean>(false)
@@ -18,95 +18,95 @@ export default () => {
 
    const displayProducts = () => showCakes || showCookies || showIceCream || showBakery || showCandyBar
 
-   const visibleCakes = () => {
-      setShowCakes(prev => !prev)
-      setShowCookies(false)
-      setShowCandyBar(false)
-      setShowBakery(false)
-      setShowIceCream(false)
-      !showCakes ? gotoElement(productsSection) : null
-   }
+  enum Products {
+    CAKES = 'Torturi',
+    COOKIES = 'Prajituri',
+    BAKERY = 'Brutarie',
+    ICE_CREAM = 'Inghetata',
+    CANDY_BAR = 'CandyBar'
+  }
 
-   const visibleCookies = () => {
-      setShowCookies(prev => !prev)
-      setShowIceCream(false)
-      setShowCakes(false)
-      setShowBakery(false)
-      setShowCandyBar(false)
-      !showCookies ? gotoElement(productsSection) : null
-   }
+  const showProducts = (category: Products) => {
+    setShowCakes(false)
+    setShowCookies(false)
+    setShowBakery(false)
+    setShowIceCream(false)
+    setShowCandyBar(false)
+     switch (category) {
+        case Products.CAKES:
+            setShowCookies(true)
+            break
+        case Products.COOKIES:
+            setShowCakes(true)
+            break
+        case Products.CANDY_BAR:
+            setShowCandyBar(true)
+            break
+        case Products.BAKERY:
+            setShowBakery(true)
+            break
+        case Products.ICE_CREAM:
+            setShowIceCream(true)
+            break
+        default:
+            break
+     }
+    gotoElement(productsSection)
+  }
 
-   const visibleCandyBar = () => {
-      setShowCandyBar(prev => !prev)
-      setShowCookies(false)
-      setShowCakes(false)
-      setShowBakery(false)
-      setShowIceCream(false)
-      !showCandyBar ? gotoElement(productsSection) : null
-   }
+  const products = [
+      {
+       name: Products.CAKES,
+       image: 'cake1.webp',
+       show: () => showProducts(Products.CAKES)
+     },
+     {
+      name: Products.COOKIES,
+      image: 'cake2.webp',
+      show: () => showProducts(Products.COOKIES)
+    },
+     {
+      name: Products.BAKERY,
+      image: 'cake3.webp',
+      show: () => showProducts(Products.BAKERY)
+    },
+     {
+      name: Products.CANDY_BAR,
+      image: 'Cheesecake.jpg',
+      show: () => showProducts(Products.CANDY_BAR)
+    },
+    {
+      name: Products.ICE_CREAM,
+      image: 'iceCream1.jpg',
+      show: () => showProducts(Products.ICE_CREAM)
+    }
+]
 
-   const visibleBakery = () => {
-      setShowBakery(prev => !prev)
-      setShowCookies(false)
-      setShowIceCream(false)
-      setShowCandyBar(false)
-      setShowCakes(false)
-      !showBakery ? gotoElement(productsSection) : null
+   const gotoElement = (section: MutableRefObject<any>) => {
+     section.current && section.current.scrollIntoView({ behavior: 'smooth' })
    }
-
-   const visibleIceCream = () => {
-      setShowIceCream(prev => !prev)
-      setShowCookies(false)
-      setShowCakes(false)
-      setShowBakery(false)
-      setShowCandyBar(false)
-      !showIceCream ? gotoElement(productsSection) : null
-   }
-
-   const categoryName = {
-      cakes: 'Torturi',
-      cookies: 'Prajituri',
-      bakery: 'Brutarie',
-      candyBar: 'Candy Bar',
-      iceCream: 'Inghetata'
-   }
-
-   const gotoElement = (section: any) => window.scrollTo({
-      top: section.current && section.current.offsetTop,
-      behavior: 'smooth'
-   })
 
    useEffect(() => {
       window.scrollTo(0, 0)
    }, [])
-
-   console.log(showCakes)
-
 
    return (
       <div className="catalog-page__wrapper">
          <NavBar/>
          <div className="catalog-page">
             <div className="catalog-page__categories">
-               <Product showProduct={visibleCakes} image={'./cake.avif'}
-                        name={categoryName.cakes}/>
-               <Product showProduct={visibleCookies} image={'./macarons.avif'}
-                        name={categoryName.cookies}/>
-               <Product showProduct={visibleCandyBar}
-                        image={'https://wp.wedme.ro/wp-content/uploads/2020/06/Flavius-Partan-056.jpg'}
-                        name={categoryName.candyBar}/>
-               <Product showProduct={visibleBakery} image={'./bakery.avif'}
-                        name={categoryName.bakery}/>
-               <Product showProduct={visibleIceCream}
-                        image={'https://www.foodiecrush.com/wp-content/uploads/2017/05/No-Churn-Ice-Cream-foodiecrush.com-31.jpg'}
-                        name={categoryName.iceCream}/>
+              {products.map(({name, image, show}, index) =>
+                <Product key={index} name={name} image={image} showProduct={show}/>)}
             </div>
+            <div ref={productsSection}>
             {displayProducts() &&
-            <div ref={productsSection}><ProductsShowcase showCakes={showCakes}
-                                                         showCookies={showCookies}
-                                                         showBakery={showBakery}
-                                                         showIceCream={showIceCream}
-                                                         showCandyBar={showCandyBar}/></div>}
+              <ProductsShowcase
+                showCakes={showCakes}
+               showCookies={showCookies}
+               showBakery={showBakery}
+               showIceCream={showIceCream}
+               showCandyBar={showCandyBar}/>}
+            </div>
          </div>
          <Footer/>
       </div>
