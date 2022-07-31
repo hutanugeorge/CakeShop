f7eb8a24d6d9fbc815ce1e6f8915ffe8d606e63c
Alex Hutanu <valexhutanu@gmail.com>
2022-07-31 15:12:09 +0300

    Created mobile responsive products

--- src/components/Categories/IceCream/IceCream.tsx
+++ src/components/Categories/IceCream/IceCream.tsx
@@ -1,5 +1,31 @@
+import ImageSlider from '../../ImageSlider'
+
+
 export default () => {
-   return (
-      <div>Hello</div>
-   )
+
+   const iceCreamImagesSlides = [
+      { url: './iceCream1.jpg', title: 'Pepene galben & Ciocolata' },
+      { url: './iceCream2.jpg', title: 'Pepene galben' },
+      { url: './iceCream3.jpg', title: 'Capsuni' }
+   ]
+
+   return <div className='ice-cream'>
+         <div className="ice-cream__image-slides">
+            <ImageSlider slides={iceCreamImagesSlides}/>
+         </div>
+         <div className="ice-cream__info">
+            <p className="ice-cream__info__title">Inghetata
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum laboriosam
+               qui recusandae rem tenetur. Aut deserunt in laudantium numquam
+               repellendus.</p>
+            <div className="ice-cream__info__prices">
+               <p className="ice-cream__info__prices__element">Cornet
+                  6 lei</p>
+               <p className="ice-cream__info__prices__element">Pahar
+                  mic 8 lei</p>
+               <p className="ice-cream__info__prices__element">Pahar
+                  mare 10 lei</p>
+            </div>
+         </div>
+   </div>
 }
--- src/components/ProductsShowcase/ProductsShowcase.tsx
+++ src/components/ProductsShowcase/ProductsShowcase.tsx
@@ -2,18 +2,12 @@ import BakeryList from '../Categories/Bakery/BakeryList'
 import CakesList from '../Categories/Cakes/CakesList'
 import CandyBar from '../Categories/CandyBar'
 import CookiesList from '../Categories/Cookies/CookiesList'
-import ImageSlider from '../ImageSlider'
+import IceCream from '../Categories/IceCream'
 import ScrollUp from '../ScrollUp'
 import ProductsProps from './interface'
 
 
 export default ({ showCakes, showCookies, showBakery, showIceCream, showCandyBar }: ProductsProps) => {
-
-   const iceCreamImagesSlides = [
-      { url: './iceCream1.jpg', title: 'Pepene galben & Ciocolata' },
-      { url: './iceCream2.jpg', title: 'Pepene galben' },
-      { url: './iceCream3.jpg', title: 'Capsuni' }
-   ]
 
    return <div className="products-showcase" >
       <ScrollUp/>
@@ -31,23 +25,7 @@ export default ({ showCakes, showCookies, showBakery, showIceCream, showCandyBar
       </div>
       <div
          className={`products-showcase__ice-cream ${showIceCream ? 'products-showcase__ice-cream__visible' : ''}`}>
-         <div className="products-showcase__ice-cream__image-slides">
-            <ImageSlider slides={iceCreamImagesSlides}/>
-         </div>
-         <div className="products-showcase__ice-cream__info">
-            <p className="products-showcase__ice-cream__info__title">Inghetata
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum laboriosam
-               qui recusandae rem tenetur. Aut deserunt in laudantium numquam
-               repellendus.</p>
-            <div className="products-showcase__ice-cream__info__prices">
-               <p className="products-showcase__ice-cream__info__prices__element">Cornet
-                  6 lei</p>
-               <p className="products-showcase__ice-cream__info__prices__element">Pahar
-                  mic 8 lei</p>
-               <p className="products-showcase__ice-cream__info__prices__element">Pahar
-                  mare 10 lei</p>
-            </div>
-         </div>
+         <IceCream />
       </div>
       <div
          className={`products-showcase__candy-bar ${showCandyBar ? 'products-showcase__candy-bar__visible' : ''}`}>
