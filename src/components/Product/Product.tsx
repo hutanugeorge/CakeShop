19f34fe7cd93c46fbe8e5aaf33a9c0f904939f47
Alex Hutanu <valexhutanu@gmail.com>
2022-08-03 23:18:57 +0300

    Changed categories photos

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,7 +4,7 @@ import ProductProps from './interface'
 export default ({showProduct, image, name}: ProductProps) => {
    return <div className="categories__element"
                onClick={showProduct}>
-      <img src={`../../../public/${image}`} alt="product image"/>
+      <img src={`./${image}`} alt="product image"/>
       <div className="categories__element__category">
          <p className="categories__element__category__name">{name}</p>
       </div>
