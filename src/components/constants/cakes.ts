e2c32096437c751383675549bb97a0e12a90d466
Alex Hutanu <valexhutanu@gmail.com>
2022-08-03 13:12:10 +0300

    Added real info about products

--- src/components/constants/cakes.ts
+++ src/components/constants/cakes.ts
@@ -1,38 +1,133 @@
 export const cakesArray = [
-    {
-        type: 'Tort insiropat',
-        image: './cake2.png',
-        info: {
-            composition: ['Lapte', 'Oua', 'Fructe', 'Bezea', 'Caramel'],
-            recommendation: 'Recomandat pentru evenimente',
-            price: 50
-        }
-    },
-    {
-        type: 'Tort insiropat',
-        image: 'cake2.png',
-        info: {
-            composition: ['123', '321'],
-            recommendation: 'Recomandat pentru evenimente',
-            price: 50
-        }
-    },
-    {
-        type: 'Tort insiropat',
-        image: 'cake2.png',
-        info: {
-            composition: ['123', '321'],
-            recommendation: 'Recomandat pentru evenimente',
-            price: 50
-        }
-    },
-    {
-        type: 'Tort insiropat',
-        image: 'cake2.png',
-        info: {
-            composition: ['123', '321'],
-            recommendation: 'Recomandat pentru evenimente',
-            price: 50
-        }
-    },
+   {
+      type: 'Tort Cifra',
+      image: './cake2.png',
+      info: {
+         composition: [ 'Blat de biscuite cu migdale', 'Crema vanilie' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 90
+      }
+   },
+   {
+      type: 'Tort Dobos',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Ciocolata', 'Unt' ],
+         recommendation: '*Decor limitat',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Egiptean',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Nuca', 'Krantz', 'Nuca caramelizata', 'Vanilie', 'Lapte', 'Unt', 'Smochine' ],
+         recommendation: '*Decor limitat',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Trio Chocolate',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Lapte', 'Ou', 'Faina', 'Cacao', 'Cafea', 'Gelatina', 'Ciocolata' +
+         ' alba/lapte/neagra' ],
+         recommendation: '*Decor limitat',
+         price: 70
+      }
+   },
+   {
+      type: 'Tort Limoncello',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina migdale', 'Fistic', 'Miere de albine', 'Unt', 'Lime' ],
+         recommendation: '*Decor limitat',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Medovic',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Miere', 'Smantana', 'Visine', 'Portocale' ],
+         recommendation: '*Decor limitat',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Carrot',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Morcov', 'Ghimbir', 'Nucsoara', 'Crema de' +
+         ' branza', 'Unt' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Cheesecake',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Biscuite cu unt', 'Crema de branza', 'Ciocolata alba', 'Piure de' +
+         ' fructe', 'Gelatina' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 90
+      }
+   },
+   {
+      type: 'Tort Entremet',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Biscuite cu migdale', 'Ciocolata', 'Crema de whisky', 'Ou', 'Mascarpone', 'Zahar' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 90
+      }
+   },
+   {
+      type: 'Tort/Rulada Pavlova',
+      image: 'cake2.png',
+      info: {
+         composition: ['Albus ou', 'Zahar', 'Amidon', 'Mascarpone', 'Crema de branza', 'Smantana', 'Fructe'],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 112
+      }
+   },
+   {
+      type: 'Tort insiropat',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Morcov', 'Ghimbir', 'Nucsoara', 'Crema de' +
+         ' branza', 'Unt' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort decorat',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Morcov', 'Ghimbir', 'Nucsoara', 'Crema de' +
+         ' branza', 'Unt' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 80
+      }
+   },
+   {
+      type: 'Tort Amandina',
+      image: 'cake2.png',
+      info: {
+         composition: ['Ou', 'Zahar', 'Faina', 'Unt', 'Ciocolata', 'Rom', 'Fondant'],
+         recommendation: '*Decor limitat',
+         price: 70
+      }
+   },
+   {
+      type: 'Tort Mozaic',
+      image: 'cake2.png',
+      info: {
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Cacao', 'Unt', 'Lamaie', 'Ciocolata' ],
+         recommendation: 'Recomandat pentru evenimente',
+         price: 60
+      }
+   },
 ]
--- src/components/constants/cookies.ts
+++ src/components/constants/cookies.ts
@@ -1,22 +1,344 @@
 export const cookiesArray = [
    {
-      type: 'Prajitura1',
-      price: 50,
+      type: 'Alba ca zapada',
+      price: 48,
       image: './index.jpg',
       info: {
-         price: 25,
-         composition: ['Lapte', 'Oua', 'Branza', 'Lapte', 'Oua', 'Branza', 'Lapte', 'Oua', 'Branza'],
+         price: 48,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Zahar', 'Lamaie', 'Ciocolata alba' ],
          recommendation: '*Recomandat pentru evenimente'
       }
    },
    {
-      type: 'Prajitura2',
+      type: 'Delicia',
       price: 50,
       image: './index.jpg',
       info: {
-         price: 24,
-         composition: ['Lapte', 'Oua', 'Branza', 'Lapte', 'Oua', 'Branza', 'Lapte', 'Oua', 'Branza'],
-         recommendation: 'Recomandat pentru evenimente'
+         price: 48,
+         composition: [ 'Ou', 'Lapte', 'Zahar', 'Unt', 'Faina', 'Vanilie', 'Cacao', 'Cocos', 'Ciocolata' +
+         ' alba' ],
+         recommendation: '*Recomandat pentru evenimente'
       }
    },
+   {
+      type: 'Snickers',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 75,
+         composition: [ 'Ou', 'Lapte', 'Faina', 'Zahar', 'Nuci', 'Unt', 'Ciocolata alba/Lapte' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Bomboane tavalite',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 53,
+         composition: [ 'Blat copt', 'Nuca', 'Unt', 'Ciocolata', 'Visine din alcool', 'Rom' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Bomboane Raffaello',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 50,
+         composition: [ 'Lapte praf', 'Unt', 'Nuca de cocos', 'Razatura de cocos', 'Migdale coapte' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Paleuri',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 55,
+         composition: [ 'Ou', 'Unt', 'Faina', 'Zahar', 'Ciocolata', 'Smantana' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Rulada Dobos',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 75,
+         composition: [ 'Ou', 'Lapte', 'Faina', 'Unt', 'Ciocolata' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Minitarte',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 68,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Vanilie/Caramel', 'Ciocolata', 'Fructe' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Prajitura cu menta',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 50,
+         composition: [ 'Ou', 'Lapte', 'Faina', 'Cacao', 'Mascarpone', 'Smantana', 'Ciocolata cu' +
+         ' menta' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Ciocolata de casa',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 75,
+         composition: [ 'Lapte praf', 'Unt', 'Cacao/Fistic/Trandafir' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Choux a la creme',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 55,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Vanilie' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Choux cu craquelin',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 60,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Vanilie' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Minieclere',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 55,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Vanilie' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Macarons',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 180,
+         composition: [ 'Faina de migdale', 'Ou', 'Zahar', 'Ciocolata', 'Diverse arome' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Saratele',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 65,
+         composition: [ 'Ou', 'Faina', 'Unt', 'Telemea', 'Chimen/Mac' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Miniamandine',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 48,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Unt', 'Ciocolata', 'Rom' ],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Prajitura Televizor',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 58,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Lamaie', 'Visine', 'Cacao' ],
+         recommendation: '*Recomandata pentru eveniment'
+      }
+   },
+   {
+      type: 'Cornulete',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 50,
+         composition: [ 'Ou', 'Faina', 'Drojdie', 'Untura', 'Rahat/Gem cu nuca' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Prajitura Tosca',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 63,
+         composition: [],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Tarta cu visine/rubarba',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 50,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Iaurt', 'Visine/Rubarba', 'Bezea' ],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Nuci',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 75,
+         composition: [ 'Ou', 'Faina', 'Zahar', 'Unt', 'Lapte', 'Nuca', 'Ciocolata', 'Rom' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Lebede',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 5,
+         composition: [ 'Ou', 'Faina', 'Lapte', 'Vanilie', 'Frisca' ],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Amandine',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: '6,3',
+         composition: ['Ou', 'Zahar', 'Faina', 'Unt', 'Ciocolata', 'Rom', 'Fondant'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Savarine',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: '6,2',
+         composition: ['Ou', 'Lapte', 'Faina', 'Drojdie', 'Smantana', 'Jeleu de fucte', 'Rom'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Ecler',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: '5,5',
+         composition: ['Ou', 'Lapte', 'Faina', 'Unt', 'Vanilie/Ciocolata'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Cheesecake',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 10,
+         composition: ['Crema de branza', 'Ciocolata alba', 'Unt de cacao', 'Gelatina/Pectina', 'Piure de fructe'],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Cuburi Bailey',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 90,
+         composition: ['Ou', 'Smantana', 'Ciocolata', 'Crema de wescy'],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Tiramisu',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 7,
+         composition: ['Ou', 'Faina', 'Lapte', 'Zahar', 'Cafea', 'Mascarpone', 'Smantana', 'Amaretto','Cacao'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Fantezie',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 12,
+         composition: ['Ou', 'Lapte', 'Unt', 'Faina de migdale', 'Cacao', 'Vanilie', 'Fructe'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Prajitura Marry',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 6,
+         composition: ['Ou', 'Faina', 'Zahar', 'Scortisoara', 'Mar', 'Smantana'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Bombite',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 5,
+         composition: ['Blat copt', 'Biscuite', 'Stafide inmuiate in rom', 'Nuca coapta', 'Ciocolata', 'Unt'],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   },
+   {
+      type: 'Sarlota',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 6,
+         composition: ['Lapte', 'Cacao', 'Gelatina', 'Rom', 'Smantana'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Duet',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: 12,
+         composition: ['Lapte', 'Ou', 'Faina', 'Cacao', 'Zahar', 'Cicolata', 'Vanilie', 'Alune'],
+         recommendation: ''
+      }
+   },
+   {
+      type: 'Turta dulce decorata',
+      price: 50,
+      image: './index.jpg',
+      info: {
+         price: '6/8/10',
+         composition: ['Ou', 'Unt', 'Lapte', 'Faina', 'Scortisoara', 'Nucsoara', 'Cuisoare', 'Royal icing'],
+         recommendation: '*Recomandat pentru evenimente'
+      }
+   }
 ]
